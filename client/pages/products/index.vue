d4fd128d513ad00f53a0c2f74f02f91a3cf0c836
ductinmdt <ductinmdt@gmail.com>
2022-01-06 15:23:35 +0700

    index pro

--- client/pages/products/index.vue
+++ client/pages/products/index.vue
@@ -44,7 +44,7 @@
 
 <script>
 export default {
-    auth: 'guest',
+    auth: 'guest auth',
     async asyncData({ $axios,params}) {
         const products = await $axios.$get(`/products/`)
         const brands = await $axios.$get('/brand/')
